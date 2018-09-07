839a7bbeeb5c3783c070a12b210d67d04149b4dd
Kelly Herstine <kelly.herstine@mymail.champlain.edu>
2018-09-07 13:47:19 -0400

    Commit  1: Adding Unity project files to repo

---
+++ AdvancedSemProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // Use this for initialization
+
+    public float speed;
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            //up
+            transform.Translate(Vector2.up * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //down
+            transform.Translate(Vector2.down * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //left
+            transform.Translate(Vector2.left * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //right
+            transform.Translate(Vector2.right * speed);
+        }
+
+
+
+    }
+}
