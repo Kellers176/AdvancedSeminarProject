4234b4c6a13779b754c2e8013cf22a291a9b19c1
Kelly Herstine <kelly.herstine@mymail.champlain.edu>
2018-09-12 18:40:01 -0400

    Added enemy movement, also added sprites into game

---
+++ AdvancedSemProject/Assets/Scripts/EnemyFollowScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowScript : MonoBehaviour {
+    public Transform target;
+    public float speed = 20.0f;
+
+    int maxHealth = 50;
+    int currentHealth;
+    private Renderer rend;
+    Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        currentHealth = maxHealth;
+        rend = GetComponent<Renderer>();
+        rend.material.color = Color.white;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (currentHealth < 40)
+        {
+            rend.material.color = Color.yellow;
+        }
+        if (currentHealth < 20)
+        {
+            rend.material.color = Color.red;
+        }
+        if (currentHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            currentHealth -= 10;
+        }
+    }
+}
--- AdvancedSemProject/Assets/Scripts/EnemyScript.cs
+++ AdvancedSemProject/Assets/Scripts/EnemyScript.cs
@@ -7,12 +7,26 @@ public class EnemyScript : MonoBehaviour {
     int maxHealth = 50;
     int currentHealth;
     private Renderer rend;
+    Rigidbody2D rb;
+
+    public GameObject pointA;
+    public GameObject pointB;
+    
+    public float speed = 20.0f;
+    
+
+    public int direction = 1;
+
+    public Quaternion left = Quaternion.Euler(0, 0, 0);
+    public Quaternion right = Quaternion.Euler(0, 180, 0);
 
 	// Use this for initialization
 	void Start () {
         currentHealth = maxHealth;
         rend = GetComponent<Renderer>();
-        rend.material.color = Color.black;
+        rend.material.color = Color.white;
+
+        rb = GetComponent<Rigidbody2D>();
     }
 	
 	// Update is called once per frame
@@ -32,6 +46,22 @@ public class EnemyScript : MonoBehaviour {
         }
        
 	}
+
+    private void FixedUpdate()
+    {
+        if(rb.position.x >= pointB.transform.position.x && direction == 1)
+        {
+            direction = -direction;
+            transform.rotation = right;
+        }
+        else if(rb.position.x < pointA.transform.position.x && direction == -1)
+        {
+            direction = -direction;
+            transform.rotation = left;
+        }
+
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
