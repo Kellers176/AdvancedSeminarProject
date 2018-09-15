6f562ffd7f601d615a4535d77ee1916bc312a0d2
Kelly Herstine <kelly.herstine@mymail.champlain.edu>
2018-09-15 17:40:03 -0400

    Working on decoupling bullet spawning

--- AdvancedSemProject/Assets/Scripts/Projectile.cs
+++ AdvancedSemProject/Assets/Scripts/Projectile.cs
@@ -2,49 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour {
+public class Projectile : MonoBehaviour
+{
 
-	public Rigidbody2D myObject;
-	public float speed = 100f;
+    public Rigidbody2D myObject;
+    private Rigidbody2D myObjectClone;
+    public float speed = 100f;
     public Vector3 myDirection;
+    int howMany = 3;
     public float[] SpreadRange;
 
 
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         myDirection = Vector3.up;
     }
-	
-	// Update is called once per frame
-	void Update () {
-            //work on player health, work on timer, work on weapon, work on fixing thsiss
-        
-		if(Input.GetKeyDown(KeyCode.Space))
-		{
-			spawnObject();
-		}
-	}
 
-	public void spawnObject()
-	{
-		Rigidbody2D myObjectClone = (Rigidbody2D) Instantiate(myObject, transform.position, transform.rotation);
-        float RandomRange = SpreadRange[Random.Range(0, SpreadRange.Length)];
-        if (myDirection == Vector3.right)
+    // Update is called once per frame
+    void Update()
+    {
+        //work on player health, work on timer, work on weapon, work on fixing thsiss
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            myObjectClone.AddForce(new Vector2(speed, RandomRange));
+            spawnObject();
         }
-        if(myDirection == Vector3.left)
-        {
-           
-            myObjectClone.AddForce(new Vector2(-speed, RandomRange));
-        }
-        if(myDirection == Vector3.up)
-        {
-            myObjectClone.AddForce(new Vector2(RandomRange, speed));
-        }
-        if(myDirection == Vector3.down)
-        {
-            myObjectClone.AddForce(new Vector2(RandomRange, -speed));
-        }
-	}
+    }
+
+    public void spawnObject()
+    {
+        
+        //myObjectClone = (Rigidbody2D)Instantiate(myObject, transform.position, transform.rotation);
+       
+
+        //float RandomRange = SpreadRange[Random.Range(0, SpreadRange.Length)];
+        //if (myDirection == Vector3.right)
+        //{
+        //    myObjectClone.AddForce(new Vector2(speed, RandomRange));
+        //}
+        //if (myDirection == Vector3.left)
+        //{
+
+        //    myObjectClone.AddForce(new Vector2(-speed, RandomRange));
+        //}
+        //if (myDirection == Vector3.up)
+        //{
+        //    myObjectClone.AddForce(new Vector2(RandomRange, speed));
+        //}
+        //if (myDirection == Vector3.down)
+        //{
+        //    myObjectClone.AddForce(new Vector2(RandomRange, -speed));
+        //}
+    }
+
+
 }
